2c8124f38c7fcdfe0d1764b162931459247d71e1
ianis-robert-moldovan <ianisrobert.moldovan@student.tsaam.be>
2025-03-18 12:01:28 +0100

    1

--- script/script.js
+++ script/script.js
@@ -5,3 +5,24 @@ const gebruikers = [
   { id: 4, login: "jos.teugels", leeftijd: 13 },
   { id: 5, login: "piet.pollekes", leeftijd: 27 },
 ];
+
+// gebruiker maken
+const login = document.querySelector("#login")
+const leeftijd = document.querySelector("#leeftijd")
+const toevoegenBtn = document.querySelector("#toevoegen")
+
+toevoegenBtn.addEventListener("click", () => {
+  console.log(login.value);
+  console.log(leeftijd.value);
+})
+
+// knop ouder
+const ouderBtn = document.querySelector("#ouderBtn")
+const filterLeeftijdBtn = document.querySelector("#filterLeeftijd")
+
+ouderBtn.addEventListener("click", () => {
+  gebruikers.filter
+})
+
+// knop jonger
+const jongerBtn = document.querySelector("#jongerBtn")
